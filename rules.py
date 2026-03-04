3c8323b3e93fa9aefd83d6e15c7c1b49d07dfb0a
Aaron Greig <whitehatag08@gmail.com>
2026-03-04 09:58:35 -0800

    Backlog Item 5 and 6 complete as well as 9 to help accomplish 5 and 6

--- rules.py
+++ rules.py
@@ -5,3 +5,16 @@ wins_against = {
     "lizard":   ["spock", "paper"],
     "spock":    ["rock", "scissors"]
 }
+
+explanation = {
+    ("rock", "scissors"): "Rock crushes Scissors",
+    ("rock", "lizard"): "Rock crushes Lizard",
+    ("paper", "rock"): "Paper covers Rock",
+    ("paper", "spock"): "Paper disproves Spock",
+    ("scissors", "paper"): "Scissors cuts Paper",
+    ("scissors", "lizard"): "Scissors decapitates Lizard",
+    ("lizard", "spock"): "Lizard poisons Spock",
+    ("lizard", "paper"): "Lizard eats Paper",
+    ("spock", "rock"): "Spock vaporizes Rock",
+    ("spock", "scissors"): "Spock smashes Scissors"
+}
