f4a69175573cc963d79a56eac594dc034141feac
Aaron Greig <whitehatag08@gmail.com>
2026-03-03 16:32:36 -0800

    Backlog Item 1 Complete

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import rps
+from rps import displayInstructions
 
 
 def main():
     keepPlaying = True
     rps.displayInstructions.called = False
-
-    while keepPlaying:
-        rps.displayInstructions()
-
-        userMove = rps.getMove()
-        compMove = rps.getCompMove()
-        rps.getWinner(userMove, compMove)
-
-        if not rps.playAgain("Do you want to play again y/n? "):
-            keepPlaying = False
-
+    displayInstructions()
     print("Thanks for playing!")
 
 
--- rps.py
+++ rps.py
@@ -1,39 +1,19 @@
-import random
-choiceWords = ["rock", "paper", "scissors"]
 def displayInstructions():
     if not displayInstructions.called:
-        print("This is the game rock, paper, scissors.")
+        print("This is the game rock, paper, scissors, lizard, spock.")
+        print("""
+These are the rules:
+
+Scissors cuts Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+(and as it always has) Rock crushes Scissors
+        """)
         print("You are playing against the computer.")
         displayInstructions.called = True
-
-def getWinner(player, comp):
-    if player == comp:
-        print("It was a tie.")
-    elif (player - comp) % 3 == 1:
-        print("You won!")
-    else:
-        print("The computer won.")
-
-def getCompMove():
-    compMove = random.randint(0, 2)
-    print(f"The computer picked {choiceWords[compMove]}.")
-    return compMove
-
-def getMove():
-    userInput = input("Please enter (R)ock, (P)aper, or (S)cissors: ")
-
-    while not userInput or userInput[0].lower() not in ['r','p','s']:
-        userInput = input("Invalid choice. Must be R, P, or S. Please try again:")
-
-    playerMove = userInput[0].lower()
-
-    for i in range(len(choiceWords)):
-        if choiceWords[i][0].lower() == playerMove:
-            return i
-
-
-def playAgain(msg):
-  answer = input(msg).lower()
-  while answer not in ['y', 'n']:
-    answer = input("Invalid response, must be y or n: ").lower()
-  return answer == 'y'
