3f6889e97159404a698948d8aa39c30bc4c3b658
Aaron Greig <whitehatag08@gmail.com>
2026-03-04 09:41:04 -0800

    Backlog Item 3 Complete

--- rps.py
+++ rps.py
@@ -1,7 +1,9 @@
+choices = ["rock", "paper", "scissors", "lizard", "spock"]
 def displayInstructions():
     if not displayInstructions.called:
         print("This is the game rock, paper, scissors, lizard, spock.")
-        print("""
+        print(
+"""
 These are the rules:
 
 Scissors cuts Paper
@@ -14,10 +16,16 @@ Lizard eats Paper
 Paper disproves Spock
 Spock vaporizes Rock
 (and as it always has) Rock crushes Scissors
-        """)
+"""
+            )
         print("You are playing against the computer.")
         displayInstructions.called = True
 
 def getUserChoice():
-    choice = input("Choose either Rock, Paper, Scissors, or Spock: ")
+    choice = ""
+
+    while choice not in choices:
+        choice = input("Choose either Rock, Paper, Scissors, or Spock: ").lower()
+        if choice not in choices:
+            print("Invalid Choice. Try again.")
     return choice
