bc1c7f3aed30aae481c1df3dd32a91ed4fa9089b
Aaron Greig <whitehatag08@gmail.com>
2026-03-04 11:12:21 -0800

    Backlog 8 complete

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ def main():
     keepPlaying = True
     rps.displayInstructions.called = False
     displayInstructions()
-    userChoice = getUserChoice()
-    computerChoice = getComputerChoice()
-    print("The computer chose: " + computerChoice +".")
-    getWinner(computerChoice,userChoice)
+    while keepPlaying:
+        userChoice = getUserChoice()
+        computerChoice = getComputerChoice()
+        print("The computer chose: " + computerChoice +".")
+        getWinner(computerChoice,userChoice)
+        playAgain = input("Would you like to keep playing?[y/n] ")
+
+        while playAgain[0].lower() not in ('y', 'n'):
+            playAgain = input("Not a valid option. Please enter y or n: ")
+
+        if playAgain[0].lower() == 'n':
+            keepPlaying = False
     print("Thanks for playing!")
 
 if __name__ == "__main__":
